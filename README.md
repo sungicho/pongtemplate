af8d44e648c156b3b153c9f398be4529520b04f3
sungicho <sungi@p6gear.com>
2025-12-22 15:25:22 -0800

    added board initialization

--- README.md
+++ README.md
@@ -1,5 +1,75 @@
-# Raylib C++ Starter Template
+# CHESS-based Resource Strategy Game
+Chess as an RTS
+Multidimensional Chess - resource vs combat; 
+OBJECTIVE: Capture Enemy King
 
-Raylib C++ Starter Template - bouncing ball (currently no collision detection with the paddle); 
-It works with raylib version 4.5.
+## Pieces:
+- Kings and Knights move identically
+- Pawns can move backwards and side to side; no longer have first move; can still only attack diagonally (but now also backward)
+- Bishops are limted to maximum 6 squares for their movement
+- Queens are limted to maximum 8 squares for their movement
+- Rooks can move 4 but attack 8; only piece that does not move when attacking
+- Only limited to 1 Queen at a time
+- Total 16 pc limit
+
+## Tech Tree
+- Pawns >> +2 ore +1 turn >> Knight
+- Knights >> +1 ore +1 turn >> Bishop
+- Pawns >> +1 ore +1 stone + 1 turn >> Bishop
+- 2 Pawns >> +2 stone +1 turn >> Rook
+- 1 Rook + Bishop >> + 2 ore + 2 turn >> Queen
+- 1 Neutral Queen + pawn in the game
+- 1 Pawn every 4 turns the queen and king are together;
+
+## PROJECT ROADMAP:
+### Gameplay (non-game mechanics)
+- [ ] moving camera pan (up and down) for larger maps
+- [X] create selector square to show square/piece selected
+- [ ] selector square to move below pieces 
+- [ ] create an info box showing node information (or piece info)
+- [ ] create an A>Z and a ## 1-20 grid pattern (like standard chess grid)
+
+
+### Game Mechanics 
+- [x] create matrix for board
+- [ ] add movement to pawn
+- [ ] add movement to knight
+- [ ] add pawn upgrade to knight
+- [ ] add resources randomly in field
+- [ ] create timer that shows queen <> king pair
+- [ ] create pawn spawn
+
+#### Opponent / AI Mechanics
+- [ ] create movement map
+- [ ] weight chart for pieces
+- [ ] pathing map for each piece
+
+
+### Controls (input)
+- [ ] make mouse movement
+- [ ] tab select pieces
+- [ ] 
+
+
+
+
+
+
+
+## General Strategy
+#### Opening
+- Rush to find ore with the Queen 
+- Avoid conflict
+- Spawn pawns
+- Try to find stone
+
+#### Mid Game
+- Try to position against enemy
+- send pawns for ore and stone
+- send  
+
+## Game Modes:
+- Short Map - 10 ores, 2 stone; 
+- Long Map - Lots of resources + start with Knight and Queen neutral queen 
+
 
--- main.cpp
+++ main.cpp
@@ -1,140 +1,167 @@
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 #include <string>
+#include <vector>
+#include <variant>
+
+// Screen sizing
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+#define TILE 48
+#define FRAME_BUFFER 15
 
 using namespace std;
 
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 800;
-const int PADDLE_EDGE_SPACE = 25;
-const int PADDLE_WIDTH = 30; 
-const int PADDLE_HEIGHT = 150;
-int playerspeed = 5; 
-int playerStart = SCREEN_HEIGHT/2;
-int rightScore = 0;
-int leftScore = 0;
+// Make this stationary board first >> then make it move so you can scan the field 
+// 32px pieces 
+const int BOARDWIDTH = 8;
+const int BOARDHEIGHT = 12;
 
 
-class Ball{
+class Ore{
 public:
-	float x, y;
-	int speed_x, speed_y; 
-	int radius;
-	void Draw(){
-		DrawCircle(x, y, radius, WHITE);
-	}
-	void Update(){
-		if(x + radius >= SCREEN_WIDTH || x - radius<= 0 )
-			{
-				if (x-radius<=0){
-					leftScore++;
-				}
-				if (x+radius >= SCREEN_WIDTH) rightScore++;
-				speed_x *= -1.16;
-			}
-        if(y + radius>= SCREEN_HEIGHT || y - radius<= 0)
-			{
-				speed_y *= -1.16;
-			}
-		x += speed_x;
-        y += speed_y;
+	Texture2D texture; 
+	int x, y; 
+void draw(int x = 100, int y = 100){
+		DrawTexture(texture, x, y, ORANGE);
 	}
 };
 
-
-class Paddle{
+class Piece{
 public:
-	float x, y; 
-	float right_x;
-	float bottom_y;
-	int score;
-	int speed_y;
-
-	void Draw(Color color){
-		// upper left corner draw;
-		DrawRectangle(x, y, PADDLE_WIDTH, PADDLE_HEIGHT, color);
-	}
-	void Update(int Up, int Down){
-		if (IsKeyDown(Down) && y<= SCREEN_HEIGHT - PADDLE_HEIGHT){
-			y += speed_y;
-			std::cout << "down" << "\n";
-		}
-		if (IsKeyDown(Up) && y >=0){
-			y -= speed_y;
-			std::cout << "up" << "\n";
-		}
+	Texture2D texture; 
+	int x, y; 
+	int itemHold; // NOTE: need to bring ore back or not? 
+void draw(int x = 100, int y = 100){
+		DrawTexture(texture, x, y, WHITE);
 	}
 };
+
+class TileCursor{
+private:
+	uint16_t blink = 600; // ms of delay in blink
+	bool blinkOn = true;
+	double lastBlink = 0; 
+public:
+	int x, y;
+	Piece piece;
+	Ore ore; 
+
+void draw(int x= 0, int y =0, Color color = PINK ){
+	if (blinkOn) DrawRectangleLines(x, y, TILE-4, TILE-4, PINK);
+		// TODO: make the cursor blink for attraction;
+		std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+		double nowDouble = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+		nowDouble = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+		double timeDelta = nowDouble - lastBlink;
+		if (timeDelta > blink){
+			blinkOn = !blinkOn;
+			lastBlink = nowDouble;
+		}
+}
+};
+
+class Board{
+public: 
+
+std::array<std::array<std::string, BOARDWIDTH>, BOARDHEIGHT> boardKey = {{}}; 
+std::array<std::array<std::variant<Piece, Ore, int>, BOARDWIDTH>, BOARDHEIGHT> boardClass = {}; 
+	int pieceCount = 0;
+	int oreCount = 0;
+
+void boardSetup(int pieceCount =2){
 	
+		for (int i= 0; i< BOARDHEIGHT; i++){
+			for (int j = 0; j<BOARDWIDTH; j++){
+				boardKey[j][i] = "0";
+			}
+		}
+	}
 
+void printBoard(){
+		std::cout<< "Board: " << "\n";
+		for (int i= 0; i< BOARDHEIGHT; i++){
+			for (int j = 0; j< BOARDWIDTH ; j++){
+				std::cout << boardKey[j][i];
+			}
+			std::cout<< "\n";
+		}
+	}
+	
+};
+
+class Background{
+public:
+	Texture2D grass;
+
+void drawPlayable (){
+	int topx = FRAME_BUFFER; 
+	int topy = FRAME_BUFFER; 
+	DrawTexture(grass, 400, 500, ORANGE);
+//rows
+	for (int i = 0; i<= BOARDHEIGHT; i++){
+		DrawLine(topx, topy + i*TILE, FRAME_BUFFER + BOARDWIDTH* TILE, topy + i*TILE, WHITE);
+	}
+	//columnns
+	for (int i = 0; i<=BOARDWIDTH; i++){
+ 		DrawLine(topx + i*TILE, topy, topx + i*TILE, FRAME_BUFFER + BOARDHEIGHT*TILE, WHITE); 
+	}
+}
+};
 
 int main () {
 
 	// initilizing ball and players
-	Paddle leftPlayer;
-	Paddle rightPlayer;
-	Ball ball;
-	leftPlayer.x = PADDLE_EDGE_SPACE; 
-	leftPlayer.y = playerStart;
-	leftPlayer.bottom_y = leftPlayer.y+PADDLE_HEIGHT;
-	rightPlayer.bottom_y = rightPlayer.y + PADDLE_HEIGHT;
-	leftPlayer.right_x = leftPlayer.x + PADDLE_WIDTH;
-	rightPlayer.right_x = rightPlayer.x + PADDLE_WIDTH;
-	rightPlayer.x = SCREEN_WIDTH - PADDLE_WIDTH - PADDLE_EDGE_SPACE;
-	rightPlayer.y = playerStart;
-	leftPlayer.speed_y = 8;
-	rightPlayer.speed_y = 8;
-	leftPlayer.score = 0;
-	ball.x = 100;
-	ball.y = 100;
-	ball.speed_x = 5;
-	ball.speed_y = 5;
-	ball.radius = 15;
-	
+	Board board; 
+	TileCursor myCursor;
+	Piece firstPawn;
+	Piece firstKing;
+	Piece firstRook;
+	Piece firstQueen;
+	Piece firstKnight;
+	Piece firstBishop;
+	Ore firstCopper;
+	Background bg;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Chess");
+	SetTargetFPS(60);
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pong >> Tetris - Sungi");
-	SetTargetFPS(120);
     while (WindowShouldClose() == false){
 		BeginDrawing();
-            ClearBackground(BLACK);
+		ClearBackground(BLACK);
+		DrawFPS(20,20); // remove if necessary; 
+		
+		// Background 
+		bg.grass = LoadTexture("../assets/grass.png"); 
+		bg.drawPlayable();
+		myCursor.draw(100,100);
 
-			// mid goal line 
-			DrawLine(SCREEN_WIDTH/2,SCREEN_HEIGHT, SCREEN_WIDTH/2, SCREEN_HEIGHT, GRAY);
-			DrawFPS(20,20); // remove if necessary; 
-			ball.Draw();
-			ball.Update();
-			leftPlayer.Draw(WHITE);
-			leftPlayer.Update(KEY_W, KEY_S);
-			rightPlayer.Draw(BLUE);
-			rightPlayer.Update(KEY_UP, KEY_DOWN);
-			// add collision detection here 
-			// check if vertical
-			// check if ball is still within X,
-			if (rightPlayer.bottom_y > ball.y - ball.radius/2 && rightPlayer.y<ball.y+ball.radius/2 
-				&& rightPlayer.x > ball.x + ball.radius/8)
-			// added 1/6th radius of buffer for paddle on the X direction 
-			// added half radius as buffer for paddle on the Y direction
-			// added time buff for debouncer (this is dependent on the X buffer math / speed)
-			{
-				ball.speed_x*=-1.1;
-				//ball.speed_y*=-1.1;
-			}
-			if (leftPlayer.bottom_y > ball.y - ball.radius/2 && leftPlayer.y<ball.y+ball.radius/2 
-					&& leftPlayer.right_x > ball.x + ball.radius/8)
-			// added 1/6th radius of buffer for paddle on the X direction 
-			// added half radius as buffer for paddle on the Y direction
-			// added time buff for debouncer (this is dependent on the X buffer math / speed)
-			{
-				ball.speed_x*=-1.1;
-				//ball.speed_y*=-1.1;
-			}
-			const char* r = to_string(rightScore).c_str();
-			const char* l = to_string(leftScore).c_str();
+		// Playable Chars
+		firstPawn.texture = LoadTexture("../assets/white-pawn48.png");
+		firstPawn.draw(FRAME_BUFFER + TILE,FRAME_BUFFER + TILE);
+		firstKing.texture = LoadTexture("../assets/white-king64.png");
+		firstKing.draw(FRAME_BUFFER+ 2*TILE,FRAME_BUFFER + 3*TILE);
+		firstKnight.texture = LoadTexture("../assets/white-knight48.png");
+		firstKnight.draw(FRAME_BUFFER + 4*TILE, FRAME_BUFFER + 4*TILE);
+		firstCopper.texture = LoadTexture("../assets/Orange3Rock.PNG");
+		firstCopper.draw(FRAME_BUFFER + 8*TILE, FRAME_BUFFER + 8*TILE);
+		board.boardSetup();
+		board.printBoard();
 
-			// Update Score
-			DrawText( r, SCREEN_WIDTH/4, SCREEN_HEIGHT - 90 , 80, WHITE);
-			DrawText( l, SCREEN_WIDTH*3/4 , SCREEN_HEIGHT - 90 , 80, RED);
 
+
+		// Will implement these later
+//		firstBishop.texture = LoadTexture("../assets/white-bishop48.png");
+//		firstBishop.draw(250,250);
+//		firstQueen.texture = LoadTexture("../assets/white-queen64.png");
+//		firstQueen.draw(300,300);
+//		firstRook.texture = LoadTexture("../assets/white-rook48.png");
+//		firstRook.draw(350,350);
         EndDrawing();
+
+		UnloadTexture(firstPawn.texture);
+		UnloadTexture(firstKing.texture);
+		UnloadTexture(firstKnight.texture);
+		UnloadTexture(bg.grass);
     }
 }
